3a4588b4ee6a2e233dca0a100fb98222d7470fc1
Yassin-Ramadan <y.ramadan1118@gmail.com>
2026-01-12 18:44:18 +0200

    implemented the factory pattern

--- src/Assignment9.sol
+++ src/Assignment9.sol
@@ -2,24 +2,31 @@
 pragma solidity ^0.8.13;
 
 // import Openzeppelin Ownable contract
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 // inherit from Ownable contract 
-contract Assignment9 {
+contract Assignment9 is Ownable {
     // create a public array called `deployedContracts` to store contracts addresses in it 
+    SimpleContract[] public deployedContracts;
 
     // call Ownable constructor of openzeppelin contract
-    constructor () {
+    constructor () Ownable(msg.sender) {
         
     }
 
     // @dev Factory to deploy new instances of `SimpleContract`
     // make only the owner is the one who can call this function
-    function createContract() external {
+    function createContract() external onlyOwner {
         // deploy new instance of SimpleContract
             // Hint: When you create a new instance, do not forget to pass any arbitrary initial value in the constructor
-        
+        SimpleContract new_contract = new SimpleContract(0);
 
         // push the new deployed instance to deployedContracts array
+        deployedContracts.push(new_contract);
+    }
+
+    function setValue(uint256 contract_index, uint256 newValue) external {
+        SimpleContract(address(deployedContracts[contract_index])).setValue(newValue);
     }
 }
 
